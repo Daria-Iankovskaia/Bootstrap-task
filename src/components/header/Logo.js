cbf7a9f0b14e7c66be284e34ef4d71d254d4b2ca
Daria Iankovskaia <d.iankovskaia@gmail.com>
2024-08-06 14:04:30 +0100

    Made logo active

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./logo.module.css";
 
 function Logo() {
     return (
-        <h2 className={`ms-3 ms-md-4 ms-lg-5 my-3 ${styles.logo}`}>World Peas</h2>
+        <Link to="/" className={styles.link}>
+            <h2 className={`ms-3 ms-md-4 ms-lg-5 my-3 ${styles.logo}`}>World Peas</h2>
+        </Link>
     );
 }
 
